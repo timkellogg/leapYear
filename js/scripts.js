830439cebb5f78d346aae4c276bbd97a40359297
Tim Kellogg & Vaughn Dice <student@epicodus.com>
2015-09-08 09:44:39 -0700

    finish leap_year

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $('.not').text('');
+    $(".year").text(year);
+    if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
